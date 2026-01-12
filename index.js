ece4b51843adc416fa25f336d559b79ea589ce59
tanishmundra-codes <tanishmundra2@gmail.com>
2026-01-12 16:34:38 +0530

    JWT added

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = "supersecretkey";
+
 
 const app = express();
 const PORT = 3000;
-const user = [];
+const users = [];
 
 
 app.use(express.json());
@@ -40,20 +43,31 @@ app.post("/login", (req, res) => {
 
     // validate
     if(!email || !password){
-        return res.status(409).json({message: "Please enter email or password"});
+        return res.status(400).json({message: "Please enter email or password"});
     }
 
     // existence
-    const existingUser = user.find(u => u.email === email);
+    const existingUser = users.find(u => u.email === email);
 
     if(!existingUser){
-       res.status(401).json({message : "User not exits"});
+       return res.status(401).json({message : "User not exits"});
     }
 
     if(existingUser.password !== password){
         return res.status(401).json({ message: "Invalid credentials" });
     }
     
+    const token = jwt.sign(
+        {email : existingUser.email},
+        JWT_SECRET,
+        {expiresIn : '1h'}
+    );
+
+    res.status(200).json({
+        message : "User loggin sucessfully",
+        token : token
+    });
+
 });
 
 app.listen(PORT, () => {
